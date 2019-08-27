3b156eb6ca5804414f2bca4e1043b5a317ce866a
Luc Verhaegen <libv@skynet.be>
2019-08-27 23:08:32 +0200

    adjust copyright dates to match reality
    Only bare kms code contains code copied from a 2011-2013 plane test
    utility.
    
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- projector.h
+++ projector.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013, 2019 Luc Verhaegen <libv@skynet.be>
+ * Copyright (c) 2019 Luc Verhaegen <libv@skynet.be>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
